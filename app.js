13aff4827b1af689af2bd0398fa2f9a693f88e52
forsaken <alexey2142@mail.ru>
2015-10-03 00:19:30 +1000

    Бекенд для products

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 var comments = require('./routes/comments');
+var products = require('./routes/products');
 
-GLOBAL.db = { comments: [ { author: 'Петя', text: 'Комментарий 1' }, { author: 'Саша', text: 'Комментарий 2' } ] };
+GLOBAL.db = { 
+  comments: [ { author: 'Петя', text: 'Комментарий 1' }, { author: 'Саша', text: 'Комментарий 2' } ],
+  products: [ { title: 'Продукт 1', count: 5 } ],
+};
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -10,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use('/comments', comments);
+app.use('/products', products);
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
