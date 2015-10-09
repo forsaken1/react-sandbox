dbe496a948387f9ba793620ac9eb46ed1223639e
Krylov Alexey <alexey2142@mail.ru>
2015-10-09 22:00:18 +1000

    Доделал продукты

--- public/src/products.jsx
+++ public/src/products.jsx
@@ -1,9 +1,22 @@
-var FilterableProductTable = React.createClass({
+var FilterableProductList = React.createClass({
+  getInitialState: function() {
+    return { data: [] };
+  },
+  componentDidMount: function() {
+    $.get(this.props.url, null, function(data) {
+      this.setState({ data: data });
+    }.bind(this));
+  },
+  handleChange: function(event) {
+    $.get(this.props.url, { search_text: event.target.value }, function(data) {
+      this.setState({ data: data });
+    }.bind(this));
+  },
   render: function() {
     return (
       <div class='filterableBlock'>
-        <SearchBar />
-        <ProductTable />
+        <SearchBar url={this.props.url} handleChange={this.handleChange} />
+        <ProductList data={this.state.data} />
       </div>
     );
   }
@@ -13,16 +26,29 @@ var SearchBar = React.createClass({
   render: function() {
     return (
       <div class='searchBlock'>
-        <input ref='search' />
+        <input ref='search' onChange={this.props.handleChange} />
       </div>
     );
   }
 });
 
-var ProductTable = React.createClass({
+var ProductList = React.createClass({
   render: function() {
+    var productRows = this.props.data.map(function(product, i) {
+      return (
+        <ProductRow iterator={i + 1} product={product} />
+      );
+    });
     return (
-
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Название</th>
+          <th>Количество</th>
+          <th>Цена</th>
+        </tr>
+        {productRows}
+      </table>
     );
   }
 });
@@ -30,7 +56,17 @@ var ProductTable = React.createClass({
 var ProductRow = React.createClass({
   render: function() {
     return (
-
+      <tr>
+        <td>{this.props.iterator}</td>
+        <td>{this.props.product.title}</td>
+        <td>{this.props.product.count}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
     );
   }
 });
+
+React.render(
+  <FilterableProductList url='/products' />,
+  document.getElementById('content')
+);
